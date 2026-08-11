5801167994198df8d81c34d8f07de43a7ed90ddf
Sankalp Dayal <sankalpdayal5@gmail.com>
2026-08-11 23:56:57 +0530

    v1.0.0: Splendor Digital Board Game (Offline AI, ISMCTS Engine, Landscape Mobile UX, AI Strategy Coach & Glassmorphism UI)

--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { soundManager } from './SoundManager.js';
-export { speechAnnouncer } from './SpeechAnnouncer.js';
-export { Haptics } from './haptics.js';
-export { useViewport } from './useViewport.js';
-export { GEM_META } from './gemMeta.js';
